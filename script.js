0606d2b365bf4cd2b6ada8016f90cf879eabf3ae
Iryna <77729246+irinyadler@users.noreply.github.com>
2021-03-12 17:03:47 +0100

    my portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+	$('.nav__burger').click(function (event) {
+		$('.nav__burger, .nav__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('.nav__menu').click(function () {
+		$('.nav__burger, .nav__menu').removeClass('active');
+		$('body').removeClass('lock');
+	});
+
+	$(window).on('scroll', function () {
+		scroll_pos = $(window).scrollTop() + $(window).height();
+		element_pos = $('.skills__row').offset().top + $('.skills__row').height();
+		if (scroll_pos > element_pos) {
+			$('.circle__percent').addClass('act');
+
+		};
+
+	})
+});
